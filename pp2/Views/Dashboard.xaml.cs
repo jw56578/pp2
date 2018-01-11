2299fa878975f451c7b36cf0b03c94372dc53a3b
Jon Woo <jw56578@gmail.com>
2018-01-10 21:37:11 -0600

    how to run code in the webview

--- pp2/Views/Dashboard.xaml.cs
+++ pp2/Views/Dashboard.xaml.cs
@@ -14,6 +14,10 @@ namespace pp2.Views
             //webview will not work unless the url is https
             string url = string.Format(Settings.DefaultUrl, Settings.Username, Settings.Password, Settings.TenancyName);
             Browser.Source = url;
+            //navigated will be called when the browser is loaded and ready
+            Browser.Navigated += (o, s) => {
+                //Browser.Eval("alert('text')");
+            };
         }
     }
 }
